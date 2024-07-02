283df98cd42dab94fad90678cb7fb7adf71a6ba3
VedangKanade <vedangk20@gmail.com>
2024-07-02 15:23:14 +0530

    Another component added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,8 @@
 // import { Flex } from "@chakra-ui/react";
-import MainPage from "./Pages/MainPage";
-
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
 import AuthPage from "./Pages/AuthPage";
 import "./App.css";
+import ChatPage from "./Pages/ChatPage";
 
 const App = () => {
   return (
@@ -13,7 +11,7 @@ const App = () => {
         <div className="flex-container">
           <Routes>
             <Route path="/" element={<AuthPage />} />
-            <Route path="/chat" element={<MainPage />} />
+            <Route path="/chat" element={<ChatPage />} />
           </Routes>
         </div>
       </BrowserRouter>
---
+++ client/src/components/Message.jsx
@@ -0,0 +1,11 @@
+import { Box } from "@chakra-ui/react";
+
+const Message = () => {
+  return (
+    <Box w="full" h="5vh" textColor="black" bgColor="white">
+      This is a message
+    </Box>
+  );
+};
+
+export default Message;
