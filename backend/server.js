8aaebc2ae735ac63bbe360ebd9b8b38297562661
VedangKanade <vedangk20@gmail.com>
2024-07-03 15:48:43 +0530

    Backend running

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+const port = 8080;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.listen(5000, () => {
+  console.log(`Server is running on port ${port}...`);
+});
