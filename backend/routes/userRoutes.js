0505471d2eb991114a567c7bacf35274576ef114
VedangKanade <vedangk20@gmail.com>
2024-07-07 23:42:03 +0530

    fixed bugs\

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { authUser } from "../controllers/userControllers";
+import { authUser } from "../controllers/userControllers.js";
 
 const router = express.Router();
 
