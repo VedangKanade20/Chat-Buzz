3b0eac00e9993f9bf39dafeab36040fc3a82795a
VedangKanade <vedangk20@gmail.com>
2024-07-12 11:41:24 +0530

    New User register backend loguc completed

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -47,4 +47,49 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile };
+/**
+ *@desc   Register new user
+ *@route  POST /api/users/
+ *@access   public
+ */
+const registerUser = asyncHandler(async (req, res) => {
+  const { firstName, lastName, email, username, password, gender, picture } =
+    req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400); //bad request
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    firstName,
+    lastName,
+    email,
+    username,
+    password,
+    gender,
+    picture,
+  });
+  if (user) {
+    //201 -success
+    res.status(201).json({
+      _id: user._id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      username: user.username,
+      email: user.email,
+      password: user.password,
+      gender: user.gender,
+      picture: user.picture,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid User data");
+  }
+});
+
+export { authUser, getUserProfile, registerUser };
