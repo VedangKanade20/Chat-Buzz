c3ecf090a251139d49613eb34caaee30ce1ef0cc
VedangKanade <vedangk20@gmail.com>
2024-09-07 15:58:46 +0530

    Socket io added at client end

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,77 @@
 // import { Flex } from "@chakra-ui/react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
 import LoginPage from "./Pages/LoginPage";
 import "./App.css";
 import ChatPage from "./Pages/ChatPage";
 import WelcomePage from "./Pages/Welcome";
 import { Flex } from "@chakra-ui/react";
 import RegisterPage from "./Pages/RegisterPage";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import io from "socket.io-client";
+import { setOtherUsers } from "./redux/userSlice";
+import { setSocket } from "./redux/socketSlice";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <WelcomePage />,
+  },
+  {
+    path: "/signup",
+    element: <RegisterPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/chat",
+    element: <ChatPage />,
+  },
+]);
 
-const App = () => {
+function App() {
+  const dispatch = useDispatch();
+  const { authUser } = useSelector((store) => store.user);
+  const { socket } = useSelector((store) => store.socket);
+
+  useEffect(() => {
+    if (authUser) {
+      const socketio = io("htto://localhost:8080", {
+        query: {
+          userId: authUser._id,
+        },
+      });
+
+      dispatch(setSocket(socketio));
+      socketio?.on("setOtherUsers", (otherUsers) => {
+        dispatch(setOtherUsers(otherUsers));
+      });
+
+      return () => socketio.close();
+    } else {
+      if (socket) {
+        socket.close();
+        dispatch(setSocket(null));
+      }
+    }
+  }, [authUser]);
   return (
     <>
-      <BrowserRouter>
-        <div className="flex-container">
-          <Flex justifyContent="center" alignItems="center">
-            <Routes>
-              <Route path="/" element={<WelcomePage />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/register" element={<RegisterPage />} />
-              <Route path="/chat" element={<ChatPage />} />
-            </Routes>
-          </Flex>
-        </div>
-      </BrowserRouter>
+      <div className="flex-container">
+        <Flex justifyContent="center" alignItems="center">
+          <RouterProvider router={router} />
+        </Flex>
+      </div>
     </>
   );
-};
+}
 
 export default App;
--- frontend/src/components/ChatArea.jsx
+++ frontend/src/components/ChatArea.jsx
@@ -1,19 +1,64 @@
-import { Flex } from "@chakra-ui/react";
+import { useDispatch, useSelector } from "react-redux";
+import { Avatar, Text, Flex, Heading } from "@chakra-ui/react";
+import Messages from "./Messages";
+import SendInput from "./SendInput";
 // import OtherUser from "./OtherUser";
 
 const ChatArea = () => {
+  const { selectedUser, authUser } = useSelector((store) => store.user);
+
+  // const dispatch = useDispatch();
   return (
-    <Flex
-      w="60vw"
-      h="80vh"
-      alignItems="center"
-      direction="column"
-      borderRadius="10px"
-      gap="5px"
-      backgroundColor="gray.900"
-    >
-      This is chat section
-    </Flex>
+    <>
+      {selectedUser !== null ? (
+        <Flex
+          direction="column"
+          minWidth={{ base: "100%", md: "550px" }}
+          flexGrow={1}
+        >
+          {/* User info section */}
+          <Flex
+            gap={2}
+            align="center"
+            bg="gray.800"
+            color="white"
+            fontWeight="semibold"
+            px={2}
+            py={2}
+            mb={2}
+          >
+            <Avatar
+              src={selectedUser?.picture}
+              size="md"
+              name={selectedUser?.username}
+            />
+            <Flex direction="column" flex={1}>
+              <Text fontWeight="semibold" fontFamily="sans-serif">
+                {selectedUser.username}
+              </Text>
+            </Flex>
+          </Flex>
+          {/* Messages and Input Section */}
+          <Messages />
+          <SendInput />
+        </Flex>
+      ) : (
+        <Flex
+          minW={{ base: "100%", md: "550px" }} // Responsive min-width
+          direction="column"
+          justify="center"
+          align="center"
+          textAlign="center"
+        >
+          <Heading as="h1" size="xl" color="white" fontWeight="bold">
+            {authUser?.username}
+          </Heading>
+          <Heading as="h2" size="lg" color="white" fontWeight="semibold">
+            Lets start a conversation
+          </Heading>
+        </Flex>
+      )}
+    </>
   );
 };
 
